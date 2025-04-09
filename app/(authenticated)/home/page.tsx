acbc0818b9f115a23aa1a946ddc4f00667abf272
Harsh Anand <80211435+nginH@users.noreply.github.com>
2025-04-10 02:02:08 +0530

    Implement HomePage component with session management and sign-out functionality

--- app/(authenticated)/home/page.tsx
+++ app/(authenticated)/home/page.tsx
@@ -1,5 +1,28 @@
-import React from "react";
-
+"use client";
+import React, { useEffect } from "react";
+import { signOut, useSession } from "next-auth/react";
+import { GithubRepos } from "@/components/github-repos";
 export default function HomePage() {
-  return <div>HomePage</div>;
+  const { data: session, status } = useSession();
+
+  const handleSignOut = async () => {
+    await signOut();
+  };
+
+  useEffect(() => {
+    console.log("HomePage rendered");
+    if (session) {
+      console.log("Session data:", session);
+    }
+  }, []);
+
+  return (
+    <div>
+      <div>HomePage</div>
+      <button onClick={handleSignOut}>Sign out</button>
+      <div className="">
+        <button onClick={() => window.location.href = "/repositories"}>List all Repositories</button>
+      </div>
+    </div>
+  );
 }
