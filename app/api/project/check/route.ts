2cb97333211177fd38e2bde663c5e563f40eedfb
Harsh Anand <80211435+nginH@users.noreply.github.com>
2025-06-23 19:13:45 +0530

    refactor: update POST handler to accept multiple project names and return all existing projects

--- app/api/project/check/route.ts
+++ app/api/project/check/route.ts
@@ -5,19 +5,29 @@ import prisma from '@/lib/prisma';
 export async function POST(request: Request) {
   try {
     const body = await request.json();
-    const { name } = body;
+    const { names } = body;
     // console.log("name", name);
-    if (!name) {
+    if (!names || names.length === 0) {
       return NextResponse.json({ error: 'Missing required fields' }, { status: 400 });
     }
-    const existingProject = await prisma.project.findFirst({
+
+    const existingProjects = await prisma.project.findMany({
       where: {
-        name,
+        name: {
+          in: Array.isArray(names) ? names : [names],
+          mode: 'insensitive',
+        },
       },
     });
+    console.log('Existing projects:', existingProjects);
     return NextResponse.json({
-      isExisting: existingProject ? true : false,
-      projectId: existingProject?.id || null,
+      isExisting: existingProjects.length > 0,
+      projects: existingProjects.map((project) => {
+        return {
+          name: project.repoName,
+          id: project.id
+        };
+      }),
     });
   } catch (error) {
     console.error('Error creating project:', error);
